a4808cb35254a51075bee2eee58eaa451f02b8b0
sudenurarslan <sudenurarslan13@gmail.com>
2025-02-01 00:42:18 +0300

    full code

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HesapMakinesi {
+public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -10,35 +10,34 @@ public class HesapMakinesi {
         System.out.print("İkinci sayıyı girin: ");
         double sayi2 = scanner.nextDouble();
 
-        double sonuc = sayi1 + sayi2;
+        System.out.print("İşlem seç (+, -, *, /): ");
+        char islem = scanner.next().charAt(0);
 
-        System.out.println("Sonuç: " + sonuc);
-        scanner.close();
-    }
-    System.out.print("İşlem seç (+ veya -): ");
-    char islem = scanner.next().charAt(0);
+        double sonuc = 0;
+        boolean gecerliIslem = true;
 
-    if (islem == '+') {
-        sonuc = sayi1 + sayi2;
-    } else if (islem == '-') {
-        sonuc = sayi1 - sayi2;
-    } else {
-        System.out.println("Hata: Geçersiz işlem!");
-    }
-}
-    else if (islem == '*') {
-    sonuc = sayi1 * sayi2;
-}
+        if (islem == '+') {
+            sonuc = sayi1 + sayi2;
+        } else if (islem == '-') {
+            sonuc = sayi1 - sayi2;
+        } else if (islem == '*') {
+            sonuc = sayi1 * sayi2;
         } else if (islem == '/') {
-        if (sayi2 != 0) {
-sonuc = sayi1 / sayi2;
-    } else {
-            System.out.println("Hata: Sıfıra bölme yapılamaz!");
+            if (sayi2 != 0) {
+                sonuc = sayi1 / sayi2;
+            } else {
+                System.out.println("Hata: Sıfıra bölme yapılamaz!");
+                gecerliIslem = false;
+            }
+        } else {
+            System.out.println("Hata: Geçersiz işlem seçtiniz!");
+            gecerliIslem = false;
+        }
+
+        if (gecerliIslem) {
+            System.out.println("Sonuç: " + sonuc);
+        }
+
         scanner.close();
-        return;
-                }
-                }
-
-
-
-                }
+    }
+}
